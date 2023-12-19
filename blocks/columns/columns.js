fb399b36b7d5a47d35a88dba05c51932f7aed5d1
Andreas Haller <andreas.haller@netcentric.biz>
2023-12-19 07:54:06 +0100

    fix linting

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,15 +1,15 @@
 export default function decorate(block) {
   const id = `gen${crypto.randomUUID().split('-')[0]}`;
   block.id = id;
-  
+
   const columns = block.querySelectorAll(':scope > div > div');
   const columnCount = columns.length;
   // following line regex matches partition sizes separated by dashes like 1-2-3
   const columnPartionRegex = /^\d{1,}(?:-\d{1,})*$/;
   const columnPartions = [...block.classList].find((c) => columnPartionRegex.test(c))?.split('-') || [];
-  
+
   let variables = '';
-  for(let i = 0; i < columnCount; i+=1) {
+  for (let i = 0; i < columnCount; i += 1) {
     const partition = columnPartions.length > i ? columnPartions[i] : 1;
     variables += `--column${i}-flex: ${partition};`;
   }
