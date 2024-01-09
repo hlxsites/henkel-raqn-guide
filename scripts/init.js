786698a7c748adbadbed69932c466ab9520e0617
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-01-09 16:24:06 +0100

    #414986 > docs

--- scripts/init.js
+++ scripts/init.js
@@ -1,21 +1,7 @@
 import ComponentLoader from './component-loader.js';
-import { config, debounce, getBreakPoint } from './libs.js';
+import { config, debounce, eagerImage, getBreakPoint } from './libs.js';
 
-export let loaded = false;
-
-export const eagerImage = (block, length = 1) => {
-  const imgs = Array.from(block.querySelectorAll('img')).slice(0, length);
-  console.log('eager', imgs, length);
-  imgs.forEach((img) => {
-    const width = img.getAttribute('width');
-    const height = img.getAttribute('height');
-    const ratio = Math.floor((width / height) * 100) / 100;
-    img.style.aspectRatio = ratio;
-    img.setAttribute('loading', 'eager');
-  });
-};
-
-export function retriveDataFrom(blocks) {
+export function getInfos(blocks) {
   return blocks.map((block) => {
     let el = block;
     const tagName = el.tagName.toLowerCase();
@@ -23,6 +9,7 @@ export function retriveDataFrom(blocks) {
     if (!config.elementBlocks.includes(tagName)) {
       [name] = Array.from(el.classList);
     } else {
+      // allow original way of defining blocks
       el = document.createElement('div');
       block.append(el);
     }
@@ -62,7 +49,7 @@ export async function init(node = document) {
     blocks = [header, ...blocks, footer];
   }
 
-  const data = retriveDataFrom(blocks);
+  const data = getInfos(blocks);
   const lcp = window.raqnLCP;
   const priority = data.filter(({ name }) => lcp.includes(name));
   const rest = data.filter(({ name }) => !lcp.includes(name));
@@ -80,12 +67,12 @@ export async function init(node = document) {
   // timeout for the rest to proper prioritize in case of stalled loading
   rest.map(({ name, el }) => setTimeout(() => start({ name, el })), 10);
 
-  // reload on breakpoint change
+  // reload on breakpoint change to reset params and variables
   window.raqnBreakpoint = getBreakPoint();
   window.addEventListener(
     'resize',
     debounce(() => {
-      // only on width changes
+      // only on width / breakpoint changes
       if (window.raqnBreakpoint !== getBreakPoint()) {
         window.location.reload();
       }
