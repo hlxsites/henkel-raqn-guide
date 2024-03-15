4763773323ed401cbef70fc79596efafbf10e8a9
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-03-15 09:54:26 +0100

    update linting rules

--- scripts/component-mixin.js
+++ scripts/component-mixin.js
@@ -1,22 +1,35 @@
 import { getMeta, loadModule } from './libs.js';
 
 export default class ComponentMixin {
-  
   static applies(element) {
-    return this.observedAttributes && [...element.attributes].map((attribute) => attribute.name)
-      .find((search) => this.observedAttributes.find((attribute) => search.startsWith(attribute) || search.startsWith(`data-${attribute}`)));
+    return (
+      this.observedAttributes &&
+      [...element.attributes]
+        .map((attribute) => attribute.name)
+        .find((search) =>
+          this.observedAttributes.find(
+            (attribute) =>
+              search.startsWith(attribute) ||
+              search.startsWith(`data-${attribute}`),
+          ),
+        )
+    );
   }
 
   static async getMixins() {
-    if(!window.raqnMixins) {
+    if (!window.raqnMixins) {
       window.raqnMixins = (async () => {
         const mixins = getMeta('mixins');
-        window.raqnMixins = await Promise.all((mixins ? mixins.split(',') : []).map(async (mixin) => {
-          const { css, js } = loadModule(`/mixins/${mixin.trim()}/${mixin.trim()}`);
-          await css;
-          const mod = await js;
-          return mod.default;
-        }));
+        window.raqnMixins = await Promise.all(
+          (mixins ? mixins.split(',') : []).map(async (mixin) => {
+            const { css, js } = loadModule(
+              `/mixins/${mixin.trim()}/${mixin.trim()}`,
+            );
+            await css;
+            const mod = await js;
+            return mod.default;
+          }),
+        );
         return window.raqnMixins;
       })();
     }
@@ -27,7 +40,7 @@ export default class ComponentMixin {
     return Promise.all(
       (await ComponentMixin.getMixins())
         .filter((mixin) => mixin.applies(element))
-        .map((Mixin) => new Mixin(element).start())
+        .map((Mixin) => new Mixin(element).start()),
     );
   }
 
@@ -36,7 +49,10 @@ export default class ComponentMixin {
   }
 
   getAttribute(name) {
-    return this.element.getAttribute(name) || this.element.getAttribute(`data-${name}`);
+    return (
+      this.element.getAttribute(name) ||
+      this.element.getAttribute(`data-${name}`)
+    );
   }
 
   async start() {
