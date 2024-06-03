9aaaf9f595d8d5cc992ee65874fb6ff4fd3e9300
Felipe Caldas <simoes.flow@gmail.com>
2024-06-03 11:45:41 +0200

    Update index.js

--- fonts/index.js
+++ fonts/index.js
@@ -1,14 +1,14 @@
 {
   "roboto": {
-    stack: "'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif",
-    options: {
+    "stack": "'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif",
+    "options": {
       "regular": "roboto-regular.woff2",
       "bold": "roboto-bold.woff2"
     }
   },
   "segoe-ui": {
-    stack: "Helvetica Neue, Helvetica, Arial, sans-serif",
-    options: {
+    "stack": "Helvetica Neue, Helvetica, Arial, sans-serif",
+    "options": {
       "regular": "segoe-ui-regular.woff",
       "bold": "segoe-ui-bold.woff",
       "italic": "segoe-ui-italic.woff"
