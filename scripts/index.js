b7d5a9d8cdf906fbf32945e611f37dced033e44b
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-09-27 19:13:22 +0200

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
 import { generateDom, manipulation, renderVirtualDom } from './render/dom.js';
-
-const start = performance.now();
 // extract all the nodes from the body
 window.raqnVirtualDom = manipulation(generateDom(document.body.childNodes));
 // clear the body
@@ -8,8 +6,7 @@ document.body.innerHTML = '';
 // append the nodes to the body after manipulation
 document.body.append(...renderVirtualDom(window.raqnVirtualDom));
 
-// callback to loadModules
+// EG callback to loadModules
 await Promise.all(window.inicialization).then(() => {
-  const end = performance.now();
-  console.log('All modules loaded initialized', end - start);
+  // some after main modules loaded
 });
